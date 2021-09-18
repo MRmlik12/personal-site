6cdc62bae137cdd6bed2354760f372fc883f87e7
MRmlik12 <mrmlik12sup@gmail.com>
2021-09-18 12:13:17 +0200

    Add fullpage support

---
+++ frontend/src/views/Contact.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from "react";
+
+export default function Contact(): ReactElement {
+  return (
+    <div className="container mx-auto content-center text-center">
+      <h1 className="text-5xl">Contact</h1>
+    </div>
+  );
+}
---
+++ frontend/src/views/Projects.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from "react";
+
+export default function Projects(): ReactElement {
+  return (
+    <div className="container mx-auto content-center text-center">
+      <h1 className="text-5xl">Projects</h1>
+    </div>
+  );
+}
