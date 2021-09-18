c8b784b681bf7cf80e21ccc9c28c949b73149f72
Daniel Olczyk <44818681+MRmlik12@users.noreply.github.com>
2021-09-18 11:09:45 +0200

    Setup toolchain for react (#1)

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  ignorePatterns: ["*.d.ts"],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'google',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint'
+  ],
+  rules: {
+    "require-jsdoc": "off",
+    "indent": "off"
+  }
+}
