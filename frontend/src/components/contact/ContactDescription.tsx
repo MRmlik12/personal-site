3c0d2d2ce20052641584fe20ecc5589d23f56416
MRmlik12 <mrmlik12sup@gmail.com>
2021-09-21 21:30:27 +0200

    Clean code with prettier

--- frontend/src/components/contact/ContactDescription.tsx
+++ frontend/src/components/contact/ContactDescription.tsx
@@ -1,11 +1,12 @@
-import React, {ReactElement} from 'react';
+import React, { ReactElement } from "react";
 
 export default function ContactDescription(): ReactElement {
   return (
-    <div className='p-10'>
+    <div className="p-10">
       <section>
-        If you want to contact me, please fill the bottom e-mail form and click send button
+        If you want to contact me, please fill the bottom e-mail form and click
+        send button
       </section>
     </div>
-  )
+  );
 }
--- frontend/src/components/contact/ContactForm.tsx
+++ frontend/src/components/contact/ContactForm.tsx
@@ -1,19 +1,24 @@
-import React, {ReactElement} from 'react';
+import React, { ReactElement } from "react";
 
 export default function ContactForm(): ReactElement {
-  const inputStyle = 'w-1/2 border-2 bg-white bg-opacity-0 mx-12 p-2 mb-5 placeholder-white placeholder-opacity-60';
+  const inputStyle =
+    "w-1/2 border-2 bg-white bg-opacity-0 mx-12 p-2 mb-5 placeholder-white placeholder-opacity-60";
 
   return (
-    <div className='block mt-16'>
+    <div className="block mt-16">
       <form>
-        <input className={inputStyle} type='text' placeholder='Title' />
-        <br/>
-        <input className={inputStyle} type='text' placeholder='E-mail address' />
-        <br/>
-        <textarea className={inputStyle} placeholder='Your message content' />
-        <br/>
-        <button className='border-2 bg-white p-1 text-black w-1/2'>Send</button>
+        <input className={inputStyle} type="text" placeholder="Title" />
+        <br />
+        <input
+          className={inputStyle}
+          type="text"
+          placeholder="E-mail address"
+        />
+        <br />
+        <textarea className={inputStyle} placeholder="Your message content" />
+        <br />
+        <button className="border-2 bg-white p-1 text-black w-1/2">Send</button>
       </form>
     </div>
-  )
+  );
 }
