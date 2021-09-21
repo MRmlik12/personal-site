8b4f64bc42ea0f1158c203afd30741e368311d45
MRmlik12 <mrmlik12sup@gmail.com>
2021-09-21 21:27:06 +0200

    Prototype contact page

--- frontend/src/views/Contact.tsx
+++ frontend/src/views/Contact.tsx
@@ -1,9 +1,13 @@
 import React, { ReactElement } from "react";
+import ContactForm from "../components/contact/ContactForm";
+import ContactDescription from "../components/contact/ContactDescription";
 
 export default function Contact(): ReactElement {
   return (
-    <div className="container mx-auto content-center text-center">
+    <div className="container mx-auto content-center text-center text-white">
       <h1 className="text-5xl">Contact</h1>
+      <ContactDescription />
+      <ContactForm />
     </div>
   );
 }
