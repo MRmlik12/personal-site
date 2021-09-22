5e9ea2579b025ca79a80e6f5aabee20eda61d547
MRmlik12 <mrmlik12sup@gmail.com>
2021-09-22 20:35:58 +0200

    Add colors to contact page

--- frontend/src/components/contact/ContactForm.tsx
+++ frontend/src/components/contact/ContactForm.tsx
@@ -2,7 +2,8 @@ import React, { ReactElement } from "react";
 
 export default function ContactForm(): ReactElement {
   const inputStyle =
-    "w-1/2 border-2 bg-white bg-opacity-0 mx-12 p-2 mb-5 placeholder-white placeholder-opacity-60";
+    "w-1/2 border-2 bg-red-400 border-red-400 bg-opacity-0 mx-12 p-2 mb-5 placeholder-white placeholder-opacity-60";
+  const textAreaStyle = `${inputStyle} h-64`;
 
   return (
     <div className="block mt-16">
@@ -19,9 +20,9 @@ export default function ContactForm(): ReactElement {
           placeholder="E-mail address"
         />
         <br />
-        <textarea className={inputStyle} placeholder="Your message content" />
+        <textarea className={textAreaStyle} placeholder="Your message content" />
         <br />
-        <button className="border-2 bg-white p-1 text-black w-1/2">Send</button>
+        <button className="border-2 bg-red-400 p-1 text-black w-1/2 border-red-400">Send</button>
       </form>
     </div>
   );
--- frontend/src/views/Contact.tsx
+++ frontend/src/views/Contact.tsx
@@ -6,7 +6,7 @@ import ContactSocialBadges from "../components/contact/ContactSocialBadges";
 export default function Contact(): ReactElement {
   return (
     <div className="container mx-auto content-center text-center text-white">
-      <h1 className="text-5xl">Contact</h1>
+      <h1 className="text-5xl font-extrabold text-red-400">Contact</h1>
       <ContactDescription />
       <ContactForm />
       <ContactSocialBadges />
