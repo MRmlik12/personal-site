b9adfe2c1474cfdce0dd99372614e5b33583fbfe
Daniel Olczyk <44818681+MRmlik12@users.noreply.github.com>
2021-09-24 22:14:12 +0200

    Add technologies page (#4)

---
+++ frontend/src/components/technologies/TechnologyList.tsx
@@ -0,0 +1,45 @@
+import React, {ReactElement} from 'react';
+import dotnetLogo from '../../assets/logos/technologies/dotnet-logo.png';
+// eslint-disable-next-line max-len
+import aspNetCoreLogo from '../../assets/logos/technologies/aspdotnetcore-logo.png';
+import efCoreLogo from '../../assets/logos/technologies/efcore-logo.png';
+import dockerLogo from '../../assets/logos/technologies/docker-logo.png';
+import vueLogo from '../../assets/logos/technologies/vue-logo.png';
+import reactLogo from '../../assets/logos/technologies/react-logo.png';
+import './TechnologyList.sass';
+
+export default function TechnologyList(): ReactElement {
+  return (
+    <div
+      // eslint-disable-next-line max-len
+      className="technologyList grid grid-flow-row gap-11 grid-cols-3 mx-auto p-10 auto-cols-auto justify-items-center"
+    >
+      <img
+        src={dotnetLogo}
+        alt="dotnet"
+        className="technology h-40 mx-10"
+      />
+      <img
+        src={aspNetCoreLogo}
+        alt="aspnetcore"
+        className="technology h-40 mx-10"
+      />
+      <img
+        src={efCoreLogo}
+        alt="efcore"
+        className="technology h-40 mx-10"
+      />
+      <img
+        src={dockerLogo}
+        alt="aspnetcore"
+        className="technology h-40 mx-10"
+      />
+      <img src={vueLogo} alt="vue" className="technology h-40 mx-10" />
+      <img
+        src={reactLogo}
+        alt="react"
+        className="technology h-40 mx-10"
+      />
+    </div>
+  );
+}
--- frontend/src/views/Technologies.tsx
+++ frontend/src/views/Technologies.tsx
@@ -1,9 +1,13 @@
 import React, {ReactElement} from 'react';
+import TechnologyList from '../components/technologies/TechnologyList';
 
 export default function Technologies(): ReactElement {
   return (
     <div className="container mx-auto content-center text-center">
-      <h1 className="text-5xl">Technologies</h1>
+      <h1 className="text-7xl font-extrabold mb-10">
+        Technologies What I&apos;m Use
+      </h1>
+      <TechnologyList />
     </div>
   );
 }
