5ffc9fb1cde3ac8d667c8350eb01b9ad42c6b23d
MRmlik12 <mrmlik12sup@gmail.com>
2021-09-27 17:22:52 +0200

    Add nodemailer to send email

--- functions/sendEmail.ts
+++ functions/sendEmail.ts
@@ -1,4 +1,5 @@
 import {Handler} from '@netlify/functions';
+import {createTransport} from 'nodemailer';
 
 export const handler: Handler = async (event, context) => {
   if (event.httpMethod !== 'POST') {
@@ -7,6 +8,37 @@ export const handler: Handler = async (event, context) => {
       message: 'Method not allowed',
     };
   }
+
+  const reqContent = JSON.parse(event.body!);
+
+  const transporter = createTransport({
+    host: process.env.SERVICE,
+    port: 587,
+    secure: true,
+    auth: {
+      user: process.env.SENDER_EMAIL,
+      pass: process.env.SENDER_PASS,
+    },
+  });
+
+  const mail = {
+    from: process.env.SENDER_EMAIL,
+    to: process.env.SEND_TO_EMAIL,
+    subject: reqContent.title,
+    text: `from: ${reqContent.senderEmail}\n content: ${reqContent.title}`,
+  };
+
+  transporter.sendMail(mail, (err, info) => {
+    if (err) {
+      console.log(err);
+      return {
+        statusCode: 200,
+        message: JSON.stringify({message: 'Couldn\'t send email'}),
+      };
+    }
+
+    console.log(info);
+  });
 
   return {
     statusCode: 200,
--- src/api/api.ts
+++ src/api/api.ts
@@ -8,7 +8,7 @@ type SendEmailResponse = {
 
 export default {
   // eslint-disable-next-line max-len
-  getSendEmailResponse: async function(title: string, senderEmail: string, description: string): Promise<AxiosResponse<SendEmailResponse>> {
+  getSendEmailResponse: async function(title: string, senderEmail: string, message: string): Promise<AxiosResponse<SendEmailResponse>> {
     return await axios.request<SendEmailResponse>({
       method: 'POST',
       url: `/sendEmail`,
@@ -16,7 +16,7 @@ export default {
       data: {
         title: title,
         senderEmail: senderEmail,
-        description: description,
+        message: message,
       },
     });
   },
