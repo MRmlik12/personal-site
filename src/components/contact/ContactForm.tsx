fda89d69409d5d0f2d1059a92ef93d21feedf39b
MRmlik12 <mrmlik12sup@gmail.com>
2021-09-28 23:50:16 +0200

    Remove AJAX solution to submit form and replace to tratitional way

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -1,70 +1,40 @@
-import React, {FormEvent, ReactElement, useState} from 'react';
-
-type FormContact = {
-  formName: string,
-  title: string,
-  email: string,
-  message: string
-}
+import React, {ReactElement} from 'react';
 
 export default function ContactForm(): ReactElement {
-  const [title, setTitle] = useState<string>('');
-  const [email, setEmail] = useState<string>('');
-  const [message, setMessage] = useState<string>('');
-
   const inputStyle =
     // eslint-disable-next-line max-len
     'w-1/2 border-2 bg-red-400 border-red-400 bg-opacity-0 mx-12 p-2 mb-5 placeholder-white placeholder-opacity-60';
   const textAreaStyle = `${inputStyle} h-64`;
 
-  const encode = (data: FormContact): string => {
-    return `form-name=${data.formName}&title=${data.title}` +
-      `&email=${data.email}&message=${data.message}`;
-  };
-
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log(title, email, message);
-    await fetch('/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: encode({
-        formName: 'contact',
-        title,
-        email,
-        message,
-      }),
-    }).catch((err) => console.log(err));
-  };
-
   return (
     <div className="block mt-12">
       <form
         id="message-form"
-        onSubmit={async (e) => await handleSubmit(e)}
+        name="contact"
         method="POST"
         data-netlify="true"
       >
         <input
           className={inputStyle}
+          name="title"
           type="text"
+          required={true}
           placeholder="Title of your message"
-          onChange={(e) => setTitle(e.target.value)}
         />
         <br />
         <input
           className={inputStyle}
+          name="email"
           type="email"
+          required={true}
           placeholder="E-mail address"
-          onChange={(e) => setEmail(e.target.value)}
         />
         <br/>
         <textarea
           className={textAreaStyle}
+          name="message"
+          required={true}
           placeholder="Your message content"
-          onChange={(e) => setMessage(e.target.value)}
         />
         <br />
         <button
