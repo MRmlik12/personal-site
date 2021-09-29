b42d58c99491d2754ebee9b0a1e5591cd83f630b
MRmlik12 <mrmlik12sup@gmail.com>
2021-09-29 19:52:00 +0200

    Add TBA to Projects

--- src/views/Projects.tsx
+++ src/views/Projects.tsx
@@ -3,7 +3,8 @@ import React, {ReactElement} from 'react';
 export default function Projects(): ReactElement {
   return (
     <div className="container mx-auto content-center text-center">
-      <h1 className="text-5xl">Projects</h1>
+      <h1 className="text-7xl">Projects</h1>
+      <h1 className="text-5xl p-4">TBA</h1>
     </div>
   );
 }
