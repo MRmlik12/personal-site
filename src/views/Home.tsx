7c67992cd55989a92402e072eb07ade288c98c88
MRmlik12 <mrmlik12sup@gmail.com>
2021-09-29 19:41:14 +0200

    Desing home page

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,9 +1,27 @@
 import React, {ReactElement} from 'react';
+import Typewriter from 'typewriter-effect';
 
 export default function Home(): ReactElement {
   return (
-    <div className="container mx-auto content-center">
-      <h1 className="text-8xl">Home</h1>
+    <div className="container w-2/5 ml-10">
+      <h1 className="text-6xl text-white font-mono">
+        Hello, My name is
+        <div className="font-bold text-yellow-600">
+          <Typewriter
+          options={{
+            strings: ['Daniel Olczyk'],
+            autoStart: true,
+            loop: true,
+          }} />
+        </div>
+        I&apos;m the young Fullstack
+        <a
+          className="text-indigo-800 font-bold tracking-wide mr-3 ml-3"
+        >
+          .NET
+        </a>
+        Developer
+      </h1>
     </div>
   );
 }
