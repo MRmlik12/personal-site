f63de4c12e1457522a2561b1fc00311f9283e477
Daniel Olczyk <44818681+MRmlik12@users.noreply.github.com>
2021-10-06 19:54:19 +0200

    Make mobile experience fixes (#9)

--- src/views/Technologies.tsx
+++ src/views/Technologies.tsx
@@ -1,10 +1,11 @@
 import React, {ReactElement} from 'react';
 import TechnologyList from '../components/technologies/TechnologyList';
+import '../components/technologies/Technologies.sass';
 
 export default function Technologies(): ReactElement {
   return (
-    <div className="container mx-auto content-center text-center">
-      <h1 className="text-7xl font-extrabold mb-10">
+    <div className="technologies container mx-auto content-center text-center">
+      <h1 className="text-7xl font-extrabold mb-10 p-1.5">
         Technologies What I&apos;m Use
       </h1>
       <TechnologyList />
