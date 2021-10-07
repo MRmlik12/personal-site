f74fa480fbf98435afbb11d05a19948a41dd239e
Daniel Olczyk <44818681+MRmlik12@users.noreply.github.com>
2021-10-07 20:36:33 +0200

    Implement form submit with formspark (#11)

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -8,27 +8,34 @@ export default function ContactForm(): ReactElement {
 
   return (
     <div className="block mt-6">
-      <form>
+      <form action="https://submit-form.com/cfA1CqTa">
         <input
           className={inputStyle}
+          name="title"
           type="text"
           placeholder="Title of your message"
+          required={true}
         />
         <br />
         <input
           className={inputStyle}
+          name="email"
           type="email"
           placeholder="E-mail address"
+          required={true}
         />
         <br/>
         <textarea
           className={textAreaStyle}
+          name="message"
           placeholder="Your message content"
+          required={true}
         />
         <br />
         <button
           className=
             "w-8/12 border-2 bg-red-400 p-1 text-black w-1/2 border-red-400"
+          type="submit"
         >
           Send
         </button>
